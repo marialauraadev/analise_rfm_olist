c2f627521c9fdd9cc8518285676252ad588ca2a7
Maria Laura Carvalho <137564457+marialauraadev@users.noreply.github.com>
2026-09-12 16:56:50 -0300

    feat(score completo): criando a coluna do RFM score completo

--- analise.py
+++ analise.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import sqlite3
+import numpy as np
 
 conexao = sqlite3.connect('dados.db')
 
@@ -16,3 +17,10 @@ df_rfm['R_score'] = pd.qcut(df_rfm['days_last_purchase'], 5, labels=[5, 4, 3, 2,
 df_rfm['F_score'] = pd.cut(df_rfm['total_purchases'], bins=[0, 3, 6, 15], labels=[1, 3, 5])
 
 df_rfm['M_score'] = pd.qcut(df_rfm['total_price_per_client'], 5, labels=[1, 2, 3, 4, 5])
+
+# utilizando colunas em string pra poder preservar as colunas originais caso seja necessário fazer alguma conta depois
+df_rfm['R_score_str'] = df_rfm['R_score'].astype('string')
+df_rfm['F_score_str'] = df_rfm['F_score'].astype('string')
+df_rfm['M_score_str'] = df_rfm['M_score'].astype('string')
+
+df_rfm['RFM_score'] = df_rfm['R_score_str'] + df_rfm['F_score_str'] + df_rfm['M_score_str']
