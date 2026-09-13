a5885fefe2012503fb7252b72fd90b0da66d9fb4
Maria Laura Carvalho <137564457+marialauraadev@users.noreply.github.com>
2026-09-13 15:39:46 -0300

    fix(extract): consertando caminho de arquivo

--- src/extract.py
+++ src/extract.py
@@ -2,9 +2,9 @@ import sqlite3
 import pandas as pd
 
 def extrair_dados():
-    conexao = sqlite3.connect('../sql/dados.db')
+    conexao = sqlite3.connect('sql/dados.db')
 
-    with open("../sql/analise_rfm.sql", "r") as analise_rfm:
+    with open("sql/analise_rfm.sql", "r") as analise_rfm:
         query = analise_rfm.read()
 
     df_rfm = pd.read_sql_query(query, conexao)
