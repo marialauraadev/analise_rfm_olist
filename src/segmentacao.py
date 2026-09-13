b76bc2e250270dfa5db7b35561ed8f1cf677c7f8
Maria Laura Carvalho <137564457+marialauraadev@users.noreply.github.com>
2026-09-13 16:52:45 -0300

    fix(segmentação): ajuste em clientes inativos e novos clientes para melhor visualização

--- src/segmentacao.py
+++ src/segmentacao.py
@@ -5,15 +5,19 @@ def gera_segmentacao(df_rfm):
         (df_rfm['R_score'] >= 4) & (df_rfm['F_score'] == 5) & (df_rfm['M_score'] >= 4),
         (df_rfm['R_score'] >= 1) & (df_rfm['R_score'] <= 3) & (df_rfm['F_score'] >= 3) & (df_rfm['M_score'] >= 4),
         (df_rfm['R_score'] >= 4) & (df_rfm['F_score'] == 3) & (df_rfm['M_score'] >= 3),
-        (df_rfm['R_score'] >= 4) & (df_rfm['F_score'] == 1),
-        (df_rfm['R_score'] >= 1) & (df_rfm['R_score'] <= 3) & (df_rfm['F_score'] == 1)
+        (df_rfm['R_score'] >= 4) & (df_rfm['F_score'] == 1) & (df_rfm['M_score'] >= 4),
+        (df_rfm['R_score'] >= 4) & (df_rfm['F_score'] == 1) & (df_rfm['M_score'] <= 3),
+        (df_rfm['R_score'] >= 1) & (df_rfm['R_score'] <= 3) & (df_rfm['F_score'] == 1) & (df_rfm['M_score'] >= 4),
+        (df_rfm['R_score'] >= 1) & (df_rfm['R_score'] <= 3) & (df_rfm['F_score'] == 1) & (df_rfm['M_score'] <= 3)
     ]
 
     classificacoes = [
         'Campeões',
         'Clientes em Risco',
         'Clientes Leais',
+        'Novos Clientes - Alto Valor',
         'Novos Clientes',
+        'Clientes Inativos - Alto Valor',
         'Clientes Inativos'
     ]
 
