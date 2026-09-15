a7a58e12b8c98057b2bfb26511d3c92fdf171be8
Maria Laura Carvalho <137564457+marialauraadev@users.noreply.github.com>
2026-09-15 18:45:34 -0300

    fix(extract): consertando caminho de diretório

--- src/extract.py
+++ src/extract.py
@@ -1,10 +1,17 @@
 import sqlite3
 import pandas as pd
+import os
 
 def extrair_dados():
-    conexao = sqlite3.connect('sql/dados.db')
+    pasta_atual = os.path.dirname(os.path.abspath(__file__))
+    pasta_raiz = os.path.dirname(pasta_atual)
 
-    with open("sql/analise_rfm.sql", "r") as analise_rfm:
+    caminho_db = os.path.join(pasta_raiz, 'sql', 'dados.db')
+    caminho_query = os.path.join(pasta_raiz, 'sql', 'analise_rfm.sql')
+
+    conexao = sqlite3.connect(caminho_db)
+
+    with open(caminho_query, "r") as analise_rfm:
         query = analise_rfm.read()
 
     df_rfm = pd.read_sql_query(query, conexao)
